e0caaf7a646f4f87392b771c8d9bfcdaf17ec048
eryk <e.szczepaniec@lightpixel.io>
2025-10-31 13:58:19 +0100

    fix(ui): center app on page, change bg color , disaply app on full screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import JokeGenerator from './layouts/JokeGenerator';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-xl font-bold text-center">🤣 Dad Joke Generator App</h1>
-      <JokeGenerator />
+    <div className="md:relative p-4 md:p-8 h-screen bg-gray-900 text-white">
+      <div className="mb-4 md:absolute md:top-1/2 md:-translate-y-1/2 md:left-1/2 md:-translate-x-1/2 max-w-xl max-w-3xl mx-auto w-[350px] md:w-3/4 md:h-[450px] text-center">
+        <h1 className="mb-4 text-xl font-bold text-center">🤣 Dad Joke Generator App</h1>
+        <JokeGenerator />
+      </div>
     </div>
   );
 };
--- src/layouts/JokeGenerator.tsx
+++ src/layouts/JokeGenerator.tsx
@@ -6,12 +6,8 @@ const JokeGenerator = () => {
   const { generatedJoke, errorMessage, handleSubmit } = useJokeGenerator();
 
   return (
-    <div className="p-2 md:p-4 rounded-lg border-2 border-gray-200 text-center">
+    <div className="p-2 md:p-4 max-w-[800px] mx-auto rounded-lg border-2 border-gray-200 text-center">
       <form onSubmit={handleSubmit}>
-        <span className="block">
-          <strong>Joke generator</strong>
-        </span>
-
         <div className="my-2 p-2 h-24 rounded-lg flex items-center justify-center bg-gray-700 text-white">
           <span className="block my-4">
             {generatedJoke ? generatedJoke : 'Are you ready for some dad jokes ?'}
