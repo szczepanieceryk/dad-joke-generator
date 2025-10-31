8570360a9f8eac8a89f7b2a24225e2ca5fd010df
eryk <e.szczepaniec@lightpixel.io>
2025-10-31 13:48:31 +0100

    feat: add main logic & UI of Joke generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import JokeGenerator from './layouts/JokeGenerator';
 
 const App = () => {
   return (
     <div>
       <h1 className="text-xl font-bold text-center">🤣 Dad Joke Generator App</h1>
+      <JokeGenerator />
     </div>
   );
 };
